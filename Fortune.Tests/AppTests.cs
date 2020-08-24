951c6650ed613b90a250399c2b09e86d3b1a5d6a
Andy Holt <andrew.holt@codecomputerlove.com>
2020-08-24 10:11:18 +0100

    Added unit tests for existing App functionality.

---
+++ Fortune.Tests/AppTests.cs
@@ -0,0 +1,49 @@
+﻿using FakeItEasy;
+using NUnit.Framework;
+
+namespace Fortune.Tests
+{
+    public class AppTests
+    {
+		private IFortuneCookie _fortuneCookie;
+		private IConsole _console;
+		private App _app;
+
+		[SetUp]
+		public void Setup()
+		{
+			_fortuneCookie = A.Fake<IFortuneCookie>();
+			_console = A.Fake<IConsole>();
+			_app = new App(_fortuneCookie, _console);
+		}
+
+		[Test]
+		public void Run_ItPromptsTheUserToEnterTheirName()
+		{
+			// Act
+			_app.Run();
+
+			// Assert
+			A.CallTo(() => _console.Write("What's your name? ")).MustHaveHappenedOnceExactly().Then(
+				A.CallTo(() => _console.ReadLine()).MustHaveHappenedOnceExactly());
+		}
+
+		[Test]
+		public void Run_GivenTheUserHasEnteredTheirName_ItGreetsTheUserAndTellsThemTheirFortune()
+		{
+			// Arrange
+			A.CallTo(() => _console.ReadLine()).Returns("Jane Doe");
+			A.CallTo(() => _fortuneCookie.GetTodaysFortune()).Returns("Certain bears do not harm humans. Today you will meet no such bears.");
+
+			// Act
+			_app.Run();
+
+			// Assert
+			A.CallTo(() => _console.WriteLine(
+				"Hi {0}!\nYour fortune for today is: {1}",
+				"Jane Doe",
+				"Certain bears do not harm humans. Today you will meet no such bears."))
+				.MustHaveHappenedOnceExactly();
+		}
+	}
+}
