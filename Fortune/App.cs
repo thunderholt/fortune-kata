e4db2e1f448498c619b4f33259f3c93540a963da
Andy Holt <andrew.holt@codecomputerlove.com>
2020-08-24 12:13:20 +0100

    Added code to pass test for user's date of birth fortune read-out.

--- Fortune/App.cs
+++ Fortune/App.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Fortune
 {
@@ -17,14 +18,23 @@ namespace Fortune
 		{
 			var name = _console.Prompt("What's your name?");
 
-			_console.Prompt("When were you born (dd/mm/yyyy)?");
+			var dobString = _console.Prompt("When were you born (dd/mm/yyyy)?");
+			DateTimeOffset.TryParseExact(dobString, "dd/MM/yyyy", null, DateTimeStyles.None, out var dob);
 
-			var person = new Person {Name = name};
+			var person = new Person
+			{
+				Name = name,
+				DateOfBirth = dob
+			};
 
 			_console.WriteLine(
 				"Hi {0}!\nYour fortune for today is: {1}",
 				person.Name,
 				_fortuneCookie.GetTodaysFortune());
+
+			_console.WriteLine(
+				"On the day you were born your fortune was: {0}",
+				_fortuneCookie.GetFortuneForDate(person.DateOfBirth));
 		}
 	}
 }
--- Fortune/Person.cs
+++ Fortune/Person.cs
@@ -1,7 +1,10 @@
-﻿namespace Fortune
+﻿using System;
+
+namespace Fortune
 {
 	public class Person
 	{
 		public string Name { get; set; }
+		public DateTimeOffset DateOfBirth { get; set; }
 	}
 }
