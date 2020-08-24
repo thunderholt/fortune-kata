36adcea55ef85ed9030edb213ef742832c2dfcf0
Andy Holt <andrew.holt@codecomputerlove.com>
2020-08-24 12:12:32 +0100

    Added failing test for user's birthday fortune read-out.

--- Fortune.Tests/AppTests.cs
+++ Fortune.Tests/AppTests.cs
@@ -1,4 +1,5 @@
-﻿using FakeItEasy;
+﻿using System;
+using FakeItEasy;
 using NUnit.Framework;
 
 namespace Fortune.Tests
@@ -29,11 +30,13 @@ namespace Fortune.Tests
 		}
 
 		[Test]
-		public void Run_GivenTheUserHasEnteredTheirName_ItGreetsTheUserAndTellsThemTheirFortune()
+		public void Run_GivenTheUserHasEnteredTheirName_ItGreetsTheUserAndTellsThemTheFortuneForTodayAndTheirBirthday()
 		{
 			// Arrange
 			A.CallTo(() => _console.Prompt("What's your name?")).Returns("Jane Doe");
+			A.CallTo(() => _console.Prompt("When were you born (dd/mm/yyyy)?")).Returns("14/06/1982");
 			A.CallTo(() => _fortuneCookie.GetTodaysFortune()).Returns("Certain bears do not harm humans. Today you will meet no such bears.");
+			A.CallTo(() => _fortuneCookie.GetFortuneForDate(A<DateTimeOffset>.That.Matches(d => d.Year == 1982 && d.Month == 6 && d.Day == 14))).Returns("Today you will experience an average 1G of gravity.");
 
 			// Act
 			_app.Run();
@@ -43,7 +46,13 @@ namespace Fortune.Tests
 				"Hi {0}!\nYour fortune for today is: {1}",
 				"Jane Doe",
 				"Certain bears do not harm humans. Today you will meet no such bears."))
-				.MustHaveHappenedOnceExactly();
+				.MustHaveHappenedOnceExactly().Then(
+
+				A.CallTo(() => _console.WriteLine(
+					"On the day you were born your fortune was: {0}",
+					"Today you will experience an average 1G of gravity."))
+					.MustHaveHappenedOnceExactly()
+			);
 		}
 	}
 }
